a9f0c0b5cb5fedeee8ec58da2905da901ee6381e
nipingel <npingel@Nickolass-MacBook-Pro.local>
2016-06-16 11:23:25 -0400

    passes sorting tests and successfully creates and resets global data buffers for a scan

--- main.py
+++ main.py
@@ -17,6 +17,10 @@ from modules.metaDataModule import MetaDataModule
 from modules.beamformingModule import BeamformingModule
 import matplotlib.pyplot as pyplot
 
+##global variables
+numGPU = 2
+numTotalThreads = numGPU*2
+
 ##TODO: How to distinguish between GPU threads, e.g. threads from 1st GPU contianing channels 0-4,100-104, and 5-9,105-109
 ##TODO:Extend sorting from 4 FITS files to 20 FITS files.
 ##TODO: how to reset the global data buffers?
@@ -55,8 +59,6 @@ def bankD(dataBuff,data,ints):
 def main():
     bf = BeamformingModule()
     md = MetaDataModule()
-    dataBuff_X = np.zeros([10,25*20])   ##TODO: make mode independent  
-    dataBuff_Y = np.zeros([10,25*20])   ##TODO: grab number of integrations from header?
     banks = {"A" : bankA,
              "B" : bankB,
              "C" : bankC,
@@ -67,15 +69,22 @@ def main():
     print('Changing working directory to: '+str(sys.argv[1]))
     print('Building Primary HDU...')
     priHeader = md.contstructPriHDUHeader   
-    for fitsName in glob.glob('/Users/npingel/Desktop/Research/FLAG/pros/exampleData/*.fits'):    
-        print('Beamforming correlations in: '+fitsName[-25:])
-        xData,yData,nints = bf.getSpectralArray(fitsName)
-        bank = fitsName[-6]
+    fitsNames = glob.glob('/Users/npingel/Desktop/Research/FLAG/pros/exampleData/*.fits')
+    cnt = 0
+    for file in fitsNames:
+        print('Beamforming correlations in: '+file[-25:])
+        if cnt % numTotalThreads == 0:
+                dataBuff_X = np.zeros([10,25*20])   ##TODO: make mode independent  
+                dataBuff_Y = np.zeros([10,25*20])   ##TODO: grab number of integrations from header?       
+        xData,yData,nints = bf.getSpectralArray(file)
+        bank = file[-6]
         bank = bank[0]##TODO: grab from header?        
         for ints in range(0,nints):
             banks.get(bank)(dataBuff_X,xData[ints,:],ints)
             banks.get(bank)(dataBuff_Y,yData[ints,:],ints)
+        cnt+=1
         ##TODO:construct actual binTbl
+    print('finished')
     
     
         
